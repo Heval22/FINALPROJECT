61dcc2ffcee71748ca0bfc02c29086d90ae2df0a
hevalnbutala <hb248@njit.edu>
2019-05-10 14:38:50 -0400

    Task: Completion

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use App\Company;
+class CompanyController extends Controller
+{
+    public function show(Company $company)
+    {
+        return view('company', compact('company'));
+    }
+}
